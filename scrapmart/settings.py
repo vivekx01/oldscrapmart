d49ad5afe0d910f6ed34b04a1a913c8819b81ba9
Vivek <72996825+vivekx01@users.noreply.github.com>
2021-06-27 21:10:47 +0530

    Update settings.py

--- scrapmart/settings.py
+++ scrapmart/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@_l=j9i8&!)zi)0le!iq^um5ndgph8-ke-zs2=41#(-kj02+v+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['scrapmart.herokuapp.com',
+'localhost:8000']
 
 
 # Application definition
