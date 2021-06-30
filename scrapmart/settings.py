0db3d6023dadbf8cd1b7b097ac56eec836b415c1
Vivek <72996825+vivekx01@users.noreply.github.com>
2021-06-30 20:11:07 +0530

    Update settings.py

--- scrapmart/settings.py
+++ scrapmart/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,6 +88,8 @@ DATABASES = {
         'HOST' : 'localhost',
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
